6ac1523d9d803064ccc5f088f35c37044908a6c7
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2024-03-10 11:58:12 +0100

    Add: Maquetación del navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import style from "./App.module.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./views/Home/Home";
-
 export default function App() {
     return (
         <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
+            <div className={style.app}>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </div>
         </Router>
     );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import style from "./Navbar.module.css";
+import { LiaBarsSolid } from "react-icons/lia";
+import { Link } from "react-router-dom";
+import { useRef, useState } from "react";
+
+export default function Navbar() {
+    const [isLinksActive, setIsLinksActive] = useState(false);
+    const links = useRef(null);
+
+    const onBars = () => {
+        setIsLinksActive(!isLinksActive);
+    };
+
+    return (
+        <nav className={style.container}>
+            <div className={style.logo}>Coffee & Co</div>
+            <div className={`${style.links} ${isLinksActive ? style.linksActive : ""}`} ref={links}>
+                <Link to="#" className={style.linksLink} onClick={onBars}>
+                    Home
+                </Link>
+                <Link to="#" className={style.linksLink} onClick={onBars}>
+                    Menu
+                </Link>
+                <Link to="#" className={style.linksLink} onClick={onBars}>
+                    Reservation
+                </Link>
+                <Link to="#" className={style.linksLink} onClick={onBars}>
+                    Blog
+                </Link>
+            </div>
+            <div className={style.profile}>
+                <Link to="#" className={style.profileLogin}>
+                    Login
+                </Link>
+                <LiaBarsSolid className={style.profileBars} onClick={onBars} />
+            </div>
+        </nav>
+    );
+}
